bea288efca6c7dfaccba4c5f893c931564a8f417
Ad4m Wayn3 <misterwayne04@gmail.com>
2026-01-12 22:55:01 -0500

    implemented enum map type, player definition moved here

--- src/root.zig
+++ src/root.zig
@@ -2,6 +2,77 @@ pub const std: type = @import("std");
 pub const rl: type = @import("raylib");
 pub const rgui: type = @import("raygui");
 
+pub const InputTag = enum { mouse, keyboard, gamepad };
+pub const Input = union(InputTag) {
+	pub const State = enum { up, down, pressed, released };
+	mouse: rl.MouseButton,
+	keyboard: rl.KeyboardKey,
+	gamepad: struct {button: rl.GamepadButton, id: i32},
+	pub fn is(self: Input, state: State) bool { return switch (state) {
+		.up => !self.is(.down),
+		.down => switch (self) {
+			.mouse => |mb| rl.isMouseButtonDown(mb),
+			.keyboard => |k| rl.isKeyDown(k),
+			.gamepad => |gp| rl.isGamepadButtonDown(gp.id, gp.button),
+		},
+		.pressed => switch (self) {
+			.mouse => |mb| rl.isMouseButtonPressed(mb),
+			.keyboard => |k| rl.isKeyPressed(k),
+			.gamepad => |gp| rl.isGamepadButtonPressed(gp.id, gp.button),
+		},
+		.released => switch (self) {
+			.mouse => |mb| rl.isMouseButtonReleased(mb),
+			.keyboard => |k| rl.isKeyReleased(k),
+			.gamepad => |gp| rl.isGamepadButtonReleased(gp.id, gp.button),
+		},
+	};}
+};
+
+pub const Player = struct {
+	x: f32, y: f32,
+	vel: rl.Vector2,
+	size: rl.Vector2,
+
+	pub fn shifted(self: *const @This(), v: rl.Vector2) @This() {
+		return .{
+			.size = self.size,
+			.vel = self.vel,
+			.x = self.x + v.x,
+			.y = self.y + v.y,
+		};
+	}
+
+	pub fn rectangle(self: *const @This()) rl.Rectangle { return .{
+		.x = self.x,
+		.y = self.y,
+		.width = self.size.x,
+		.height = self.size.y,
+	};}
+
+	pub fn pos(self: *@This()) rl.Vector2 { return .{.x = self.x, .y = self.y}; }
+};
+
+pub fn EnumMap(EnumKey: type, Value: type) type { return struct {
+    pub const count = @typeInfo(EnumKey).@"enum".fields.len;
+    const Self = @This();
+    pub const Pair = struct {EnumKey, Value};
+    values: [count]Value,
+    pub fn new(pairs: *const [count]Pair) Self {
+        var self: Self = undefined;
+        inline for (pairs) |pair| {
+            const key, const value = pair;
+            self.values[@intFromEnum(key)] = value;
+        }
+        return self;
+    }
+    pub fn at(self: *Self, key: EnumKey) Value {
+        return self.values[@intFromEnum(key)];
+    }
+    pub fn @"&"(self: *Self, key: EnumKey) *Value {
+        return &self.values[@intFromEnum(key)];
+    }
+};}
+
 pub inline fn isInRectangle(rec: rl.Rectangle, v: rl.Vector2) bool {
     return v.x >= rec.x and v.x <= rec.x + rec.width and v.y >= rec.y and v.y <= rec.y + rec.height;
 }
@@ -17,6 +88,18 @@ pub inline fn rectangleV(v: rl.Vector2, u: rl.Vector2) rl.Rectangle {
 
 pub inline fn rectangleTipTail(v: rl.Vector2, u: rl.Vector2) rl.Rectangle {
     return rl.Rectangle{ .x = @min(v.x, u.x), .y = @min(v.y, u.y), .width = @abs(u.x - v.x), .height = @abs(u.y - v.y) };
+}
+
+pub fn screenV() rl.Vector2 { return .{
+	.x = @floatFromInt(rl.getScreenWidth()),
+	.y = @floatFromInt(rl.getScreenHeight()),
+};}
+
+/// Draws a vector with specified thickness and `color` in the center of
+/// the screen. Should be called outside of a `Mode2D` context.
+pub fn drawVecCentered(thick: f32, color: rl.Color, vec: rl.Vector2) void {
+    const origin = screenV().scale(0.5);
+	rl.drawLineEx(origin, origin.add(vec), thick, color);
 }
 
 pub fn translate(comptime T: type, v: rl.Vector2, x: T) T {
