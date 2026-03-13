fc6def80b682c9bab048a1fccb2b5a27a05c00fe
Ad4m Wayn3 <misterwayne04@gmail.com>
2026-03-12 22:37:39 -0500

    testing deadzone camera implementation

--- src/main.zig
+++ src/main.zig
@@ -11,10 +11,6 @@ var platformer: Platformer = undefined;
 var printBuffer: [0x200]u8 = undefined;
 
 pub fn main() !void {
-    try editor.init(allocator);
-    defer editor.deinit(allocator);
-    try platformer.init(allocator, editor.map.items, &printBuffer);
-
     rl.setTraceLogLevel(.warning);
     rl.setExitKey(.null);
     rl.setTargetFPS(60);
@@ -28,6 +24,10 @@ pub fn main() !void {
     x /= [_]i32{5}**2;
     rl.traceLog(.info, "calculated resolution: %dx%d\n", .{x[0],x[1]});
     rl.setWindowSize(x[0], x[1]);}
+
+    try editor.init(allocator);
+    defer editor.deinit(allocator);
+    try platformer.init(allocator, editor.map.items, &printBuffer);
     
     rgui.setStyle(.default, .{ .default = .text_size }, 21);
     var mode: enum(u2) { platformer, editor } = .editor;
@@ -57,51 +57,5 @@ pub fn main() !void {
             const tsize = rgui.getStyle(.default, .{ .default = .text_size });
             rgui.setStyle(.default, .{ .default = .text_size }, tsize - 1);
         }
-    }
-}
-
-const State = struct {
-    box1: rl.Rectangle = .{.x = 200, .y = 200, .width = 100, .height = 100},
-    box2: rl.Rectangle = .{.x = 340, .y = 200, .width = 100, .height = 100},
-    camera: rl.Camera2D = .{.target = .{.x = 0, .y = 0},
-        .offset = .{.x=0, .y=0}, .rotation = 0, .zoom = 1},
-    collide: bool = false,
-    cursorDragging: bool = false,
-};
-
-var state = State {};
-
-pub fn _main() !void {
-    rl.setTraceLogLevel(.warning);
-    //rl.setTargetFPS(20);
-    rl.initWindow(1600,900,"collision test!");
-    defer rl.closeWindow();
-    while (!rl.windowShouldClose()) {
-        const curPos = rl.getMousePosition().add(state.camera.target);
-        if (rl.isMouseButtonPressed(.left) and rlzig.isInRectangle(state.box1, curPos)) {
-            state.cursorDragging = true;
-        }
-
-        if (rl.isMouseButtonDown(.left) and state.cursorDragging) {
-            const shift = rl.getMouseDelta();
-            state.box1.x += shift.x;
-            state.box1.y += shift.y;
-        } else state.cursorDragging = false;
-
-        if (rgui.button(.{.x=30,.y=30,.width=300,.height=40}, "toggle colissions"))
-            state.collide = state.collide != true;
-        
-        if (state.collide and rl.checkCollisionRecs(state.box1, state.box2))
-            rlzig.fixCollisionRecs(&state.box1, state.box2);
-
-        rl.beginDrawing(); {
-        rl.clearBackground(.{.r = 0, .g = 50, .b = 40, .a = 255});
-        rl.drawRectangleRec(state.box2, .white);
-        rl.drawRectangleRec(state.box1, .gray);
-
-        if (rl.checkCollisionRecs(state.box1, state.box2)) {
-            rl.drawRectangleLinesEx(state.box1, 3, .red);
-        }
-        } rl.endDrawing();
     }
 }
